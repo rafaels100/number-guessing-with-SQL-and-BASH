a19a0af6b88668715ae976df474065091f1c84eb
rafaels100 <rafamontero@live.com>
2024-07-29 16:19:12 -0300

    feat: crear logica de juego number guessing

--- number_guessing.sh
+++ number_guessing.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+#Para generar un numero random entre 1 y 10
+RAND=$((1 + $RANDOM % 10))
+#veo si el usuario lo adivino
+function MAIN() {
+	if [[ $1 ]]; then
+		echo "Lo siento ese no es el numero. Intentelo de nuevo. Ingrese enter para dejar de jugar"
+	fi
+	read NUMERO
+	#si el usuario no ingresa nada, se termina el juego
+	if [[ -z $NUMERO ]]; then 
+		exit
+	fi
+	if [[ $NUMERO = $RAND ]]; then
+		#si el usuario adivina, se da el mensaje y se termina la funcion
+		echo "Lo has adivinado! Es $NUMERO"
+	else
+		#en caso de no adivinar, se vuelve a llamar a la funcion con un argumento cualquiera para que se muestre el mensaje del principio de la funcion
+		MAIN 1
+	fi
+}
+MAIN
